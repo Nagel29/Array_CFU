55b48fbd5b1da2ff93a292d0f843cd2c0a771794
Ryan Nagel <RNagel29@gmail.com>
2022-07-09 12:56:08 -0600

    Add 3 call methods

--- arrays.js
+++ arrays.js
@@ -1,3 +1,12 @@
 var dogs = ["Lou", "Kya", "Jordi", "Misha"]
 var ages = [10, 2, 3, 4]
 var myDog = [true, true, false, false]
+
+//this will add the 2 elements at the end of the dogs array
+dogs.push("Toby", "Penny")
+
+//this will remove the "4" element at the end of the ages array
+ages.pop()
+
+//this will remove the first element ("true") in the myDog array
+myDog.shift()
